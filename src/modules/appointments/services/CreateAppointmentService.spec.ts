21e996a902d576763f585aaa59a884331c467532
RoMartins <rodrigomartins_santos1804@hotmail.com>
2020-11-23 23:59:34 -0300

    TEST create a new appointments on same date

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -18,5 +20,25 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {});
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '484',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '484',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
