ea5962c59851a143ea5509b6f6339cf559f41af4
RoMartins <rodrigomartins_santos1804@hotmail.com>
2021-01-05 22:53:13 -0300

    ListProviderDayAvailability test

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -18,12 +18,16 @@ describe('ListProviders', () => {
   it('should be able to list the month availability from providers', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2020, 4, 20, 15, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 9, 0, 0),
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProvidersDayAvailability.execute({
@@ -37,7 +41,9 @@ describe('ListProviders', () => {
       expect.arrayContaining([
         { hour: 8, available: false },
         { hour: 9, available: false },
-        { hour: 10, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
