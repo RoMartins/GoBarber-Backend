13c99aab9b9bc5b53dfae5ae2a16e5928b489802
RoMartins <rodrigomartins_santos1804@hotmail.com>
2021-01-24 22:42:10 -0300

    RateLimiter

--- src/shared/Infra/http/server.ts
+++ src/shared/Infra/http/server.ts
@@ -9,9 +9,11 @@ import uploadConfig from '../../../config/upload';
 import AppError from '../../errors/AppError';
 import '@shared/infra/typeorm/index';
 import '@shared/container';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
